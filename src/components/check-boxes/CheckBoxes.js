f7bcf469e2d944faddc56536b8b02bd2875f9ac8
mikechibuzor <iwuagwuchibuzor@gmail.com>
2022-09-17 18:51:31 +0100

    installed react prop-types for validations

--- src/components/check-boxes/CheckBoxes.js
+++ src/components/check-boxes/CheckBoxes.js
@@ -1,6 +1,7 @@
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { PropTypes } from "prop-types";
 
 const CheckBoxes = ({
   setPasswordStrengthCounter,
@@ -17,65 +18,69 @@ const CheckBoxes = ({
   };
   return (
     <>
-      <FormGroup className='text-gray-300 w-full mt-2'>
+      <FormGroup className="text-gray-300 w-full mt-2">
         <FormControlLabel
           control={
             <Checkbox
               onChange={toggleCheckbox}
               sx={{
-                color: 'rgb(209 213 219)',
-                '&.Mui-checked': {
-                  color: 'rgb(134 239 172)',
+                color: "rgb(209 213 219)",
+                "&.Mui-checked": {
+                  color: "rgb(134 239 172)",
                 },
               }}
             />
           }
-          label='Include Uppercase Letters'
+          label="Include Uppercase Letters"
         />
         <FormControlLabel
           control={
             <Checkbox
               onChange={toggleCheckbox}
               sx={{
-                color: 'rgb(209 213 219)',
-                '&.Mui-checked': {
-                  color: 'rgb(134 239 172)',
+                color: "rgb(209 213 219)",
+                "&.Mui-checked": {
+                  color: "rgb(134 239 172)",
                 },
               }}
             />
           }
-          label='Include Lowercase Letters'
+          label="Include Lowercase Letters"
         />
         <FormControlLabel
           control={
             <Checkbox
               onChange={toggleCheckbox}
               sx={{
-                color: 'rgb(209 213 219)',
-                '&.Mui-checked': {
-                  color: 'rgb(134 239 172)',
+                color: "rgb(209 213 219)",
+                "&.Mui-checked": {
+                  color: "rgb(134 239 172)",
                 },
               }}
             />
           }
-          label='Include Numbers'
+          label="Include Numbers"
         />
         <FormControlLabel
           control={
             <Checkbox
               onChange={toggleCheckbox}
               sx={{
-                color: 'rgb(209 213 219)',
-                '&.Mui-checked': {
-                  color: 'rgb(134 239 172)',
+                color: "rgb(209 213 219)",
+                "&.Mui-checked": {
+                  color: "rgb(134 239 172)",
                 },
               }}
             />
           }
-          label='Include Symbols'
+          label="Include Symbols"
         />
       </FormGroup>
     </>
   );
+};
+CheckBoxes.propTypes = {
+  setPasswordStrengthCounter: PropTypes.number,
+  passwordStrengthCounter: PropTypes.number,
 };
 export default CheckBoxes;
--- src/components/password-strength/PasswordStrengh.js
+++ src/components/password-strength/PasswordStrengh.js
@@ -1,15 +1,24 @@
+import { PropTypes } from "prop-types";
 const PasswordStrength = ({ passwordStrengthCounter }) => {
   return (
     <>
-      <div className='w-full flex items-center justify-between text-gray-500 uppercase bg-black px-5 py-4 mt-4 font-medium'>
+      <div className="w-full flex items-center justify-between text-gray-500 uppercase bg-black px-5 py-4 mt-4 font-medium">
         <p>Strength</p>
-        <div className='flex items-center gap-x-2'>
+        <div className="flex items-center gap-x-2">
           {Array.from(Array(4).keys()).map((item, index) => (
-            <div key={item} className={`border px-1 py-3  ${ index <= passwordStrengthCounter ? 'bg-yellow-600' : ''}` }></div>
+            <div
+              key={item}
+              className={`border px-1 py-3  ${
+                index <= passwordStrengthCounter ? "bg-yellow-600" : ""
+              }`}
+            ></div>
           ))}
         </div>
       </div>
     </>
   );
+};
+PasswordStrength.propTypes = {
+  passwordStrengthCounter: PropTypes.number,
 };
 export default PasswordStrength;
